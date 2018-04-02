86091783e4980c5a8b37443e950e404e09176d2f
Sean Francis N. Ballais <sean@seanballais.com>
2018-04-02 22:50:32 +0800

    Part 1 complete.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,18 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    if (params.has_key? :sort)
+      if (params[:sort] == "title")
+        @movies = Movie.order(:title)
+        @sort_type = :title
+      elsif (params[:sort] == "date")
+        @movies = Movie.order(:release_date)
+        @sort_type = :date
+      end
+    else
+      @movies = Movie.all
+      @sort_type = nil
+    end
   end
 
   def new
